60c4a434f5e90e9707b7744ba4f4f243708169d7
DiegoEstebanPerezMejia <dperezm2000@alumno.ipn.mx>
2026-06-14 19:15:41 -0600

    Add files via upload

---
+++ prueba.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int diez = 9;
+    float decimal = 1.9;
+
+    printf("Arturo %f\n", decimal);
+
+    scanf("%d", &diez);
+
+    printf("%d", diez);
+
+    return 0;
+}
